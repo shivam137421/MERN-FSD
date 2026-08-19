fa4d294cdbe3b8270afefe64b5c5a08841aca0ca
shivam137421 <sy6220639@gmail.com>
2026-08-19 07:38:33 +0530

    changed folder name from react-tutorials to 01-folder for clear folder structures

---
+++ Reactjs/01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>parent1
+        <div>child1</div>
+        <div>child2</div>
+      </div>
+      <div>parent2</div>
+    </>
+  )
+}
+
+export default App
