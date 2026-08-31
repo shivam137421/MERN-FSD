7261801e9fc6436a6c0548b63699e8115d063dc7
shivam137421 <sy6220639@gmail.com>
2026-08-31 20:52:44 +0530

    used http methods

--- nodejs/server/index.js
+++ nodejs/server/index.js
@@ -2,21 +2,37 @@ const http = require("http");
 const  fs = require("fs");
 const url = require('url');
 
+// const myserver = http.createServer((req,res)=>{
+//     console.log(req.url);
+//     res.end("server responded");
+// })
+
 const myserver = http.createServer((req,res)=>{
     if(req.url === '/favicon.ico'){
         return res.end();
     }
-    const log = `${Date.now()}:${req.url} new request recieved\n`;
+    // console.log(req);
+    const log = `${Date.now()}: ${req.method} ${req.url} new request recieved\n`;
     const myurl = url.parse(req.url,true);
     console.log(myurl);
     fs.appendFile('log.txt',log,(err,data)=>{
         switch(myurl.pathname){
-            case "/": 
-            res.end("homepage");
-            break;
+            case "/":
+                if(req.method==='GET'){
+                    res.end("homepage");
+                } 
+                break;
             case "/about":
-                 res.end(`hii ${myurl.query.myname}`);
-            break;
+                res.end(`hii ${myurl.query.myname}`);
+                break;
+            case "/signup":
+                if(req.method==='GET'){
+                    res.end("this is a signup form");
+                }
+                else if(req.method==='POST'){
+                    res.end("submitted successfully");
+                }
+                break;    
             default: res.end("404 not found");
         }
     })
